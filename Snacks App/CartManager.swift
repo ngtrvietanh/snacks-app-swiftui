9c13d148f05e991d4642c00e88eb6b83dc0a7d5e
Trần Việt Anh <tranvietanhcbh@gmail.com>
2024-12-03 15:24:45 +0700

    completed

---
+++ Snacks App/CartManager.swift
@@ -0,0 +1,18 @@
+//
+//  CartManager.swift
+//  Snacks App
+//
+//  Created by Việt Anh on 3/12/24.
+//
+
+import SwiftUI
+
+struct CartManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartManager()
+}
